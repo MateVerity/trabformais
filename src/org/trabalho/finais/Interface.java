593f48797432d20ac8bfcf9374717b5b6649cc96
Mateus Nunes Campos <mncampos@inf.ufrgs.br>
2020-10-20 03:03:52 -0300

    bb

--- src/org/trabalho/finais/Interface.java
+++ src/org/trabalho/finais/Interface.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class Interface {
 
-    public static Button
+
 
 
 }
--- src/org/trabalho/finais/Main.java
+++ src/org/trabalho/finais/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -13,15 +14,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{  //Código de inicialização padrão do programa
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Trabalho Final");
-        primaryStage.setScene(new Scene(root, 1080, 600));
-        primaryStage.getIcons().add(new Image("appicon.png"));
-        primaryStage.show();
-        //Inicio das operações
         File AutomataFile = Operacoes.GetAutomataFile(primaryStage); //Variável que recebe o arquivo contendo o AFD
         Automata AFD = Operacoes.AutomataReader(AutomataFile);
-        AFD.printaAutomato();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Trabalho Final");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.getIcons().add(new Image("appicon.png"));
+        primaryStage.show();
+
+
+
+        //Inicio das operações
+
 
 
 
